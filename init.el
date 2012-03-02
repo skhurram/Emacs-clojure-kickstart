63c7c1cf8d3464af3014622ec37190acb72235fe
John Stevenson (jr0cket) <john@jr0cket.co.uk>
2012-03-02 15:50:45 +0000

    Updated init.el to add clojure support

--- init.el
+++ init.el
@@ -3,5 +3,23 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+	'("marmalade" . "http://marmalade-repo.org/packages/") )
 (package-initialize)
+
+
+;; Add Clojure and other supporting packages to the Emacs environment
+;; Packages are installed if they are not already present
+;; The list includes packages for the starter kit, Clojure and markdown files (used by github)
+
+(when (not package-archive-contents)
+ 	(package-refresh-contents))
+
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-eshell starter-kit-bindings
+	clojure-mode clojure-test-mode
+	markdown-mode ))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+
